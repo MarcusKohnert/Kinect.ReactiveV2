a413246d4517dcf138b3822f8e6d1c9773545697
Marcus Kohnert <marcus.dev@live.com>
2014-08-30 12:47:50 +0200

    Added package scripts

---
+++ .nuget/packageKinectReactiveV2Input.ps1
@@ -0,0 +1,12 @@
+param([string]$version)
+
+if([System.String]::IsNullOrEmpty($version))
+{
+	Write-Warning("No version specified")
+	Exit
+}
+
+Split-Path -Parent $dte.Solution.FileName | cd
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:Rebuild /p:Configuration=Release "Kinect.ReactiveV2.sln"
+
+nuget pack ".\nugetDrop\Kinect.ReactiveV2.Input\Kinect.ReactiveV2.Input.nuspec" -OutputDirectory ".\nugetDrop\Kinect.ReactiveV2.Input" -Version $version
